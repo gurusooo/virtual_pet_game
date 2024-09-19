295f10799c17f0f55596c5e3bbbe07ae0d65b1ea
gurusooo <130143477+gurusooo@users.noreply.github.com>
2024-09-19 18:24:35 +0300

    Create game.js

---
+++ game.js
@@ -0,0 +1,22 @@
+function setName() {
+  let name = document.getElementById('cat-name').value;
+  if (name) {
+      Module._set_cat_name(name);
+  }
+}
+
+function feedCat() {
+  Module._feed_cat();
+}
+
+function playCat() {
+  Module._play_cat();
+}
+
+function groomCat() {
+  Module._groom_cat();
+}
+
+function leaveCat() {
+  Module._leave_cat();
+}
