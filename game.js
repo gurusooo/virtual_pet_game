6e69fa8a6b349ca0f9530995c21f9659d0f9c78e
gurusooo <130143477+gurusooo@users.noreply.github.com>
2024-09-20 14:31:24 +0300

    Update game.js

--- game.js
+++ game.js
@@ -1,22 +1,22 @@
-function setName() {
-  let name = document.getElementById('cat-name').value;
-  if (name) {
-      Module._set_cat_name(name);
+  function setName() {
+      let name = document.getElementById('cat-name').value;
+      if (name) {
+          Module._set_cat_name(name);
+      }
   }
-}
 
-function feedCat() {
-  Module._feed_cat();
-}
+  function feedCat() {
+      Module._feed_cat();
+  }
 
-function playCat() {
-  Module._play_cat();
-}
+  function playCat() {
+      Module._play_cat();
+  }
 
-function groomCat() {
-  Module._groom_cat();
-}
+  function groomCat() {
+      Module._groom_cat();
+  }
 
-function leaveCat() {
-  Module._leave_cat();
-}
+  function leaveCat() {
+      Module._leave_cat();
+  }
